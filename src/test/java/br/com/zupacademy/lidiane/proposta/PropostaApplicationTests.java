5ad86493838abb3724bf81084bbee4c733ba981e
unknown <santoslid@gmail.com>
2021-05-13 10:26:27 -0300

    setup do projeto

---
+++ src/test/java/br/com/zupacademy/lidiane/proposta/PropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.lidiane.proposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
